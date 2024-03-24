47ae5d1f11861a5c80056bc44e34d6e9d2e59be8
Youcef Harmouni <xaredev@gmail.com>
2024-03-24 20:25:16 +0100

    added Carousel + fixed font issue + organaized the files + added splide lib

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var splide = new Splide('.splide', {
+        type:"loop",
+        width: '100%',
+        height:"100vh"
+    }).mount();
+} );
