39da0381b68c7dae49c133630d4c5df4bf0f6f39
Youcef Harmouni <xaredev@gmail.com>
2024-03-25 23:00:43 +0100

    Fixed the issue with the carousel won't work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     var splide = new Splide('.splide', {
         type:"slide",
         width: '100%',
         height:"100vh",
         autoplay:"enable",
         rewind:true,
-        interval: 2500,
         cover:true
     }).mount();
 } );
